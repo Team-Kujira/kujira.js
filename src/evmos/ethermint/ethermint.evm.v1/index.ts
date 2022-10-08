06ec0959d5d97365a804668e2b53b4001a5db2f1
codehans <94654388+codehans@users.noreply.github.com>
2022-10-08 10:13:18 +0100

    Pagination

---
+++ src/evmos/ethermint/ethermint.evm.v1/index.ts
@@ -0,0 +1,19 @@
+// THIS FILE IS GENERATED AUTOMATICALLY. DO NOT MODIFY.
+
+import { Registry, EncodeObject } from "@cosmjs/proto-signing";
+import { Api } from "./rest";
+import { MsgEthereumTx } from "./types/ethermint/evm/v1/tx";
+
+export const types = [["/ethermint.evm.v1.MsgEthereumTx", MsgEthereumTx]];
+export const MissingWalletError = new Error("wallet is required");
+
+export const registry = new Registry(<any>types);
+
+export const txClient = {
+  msgEthereumTx: (data: MsgEthereumTx): EncodeObject => ({
+    typeUrl: "/ethermint.evm.v1.MsgEthereumTx",
+    value: MsgEthereumTx.fromPartial(data),
+  }),
+};
+
+export { Api };
---
+++ src/evmos/ethermint/ethermint.feemarket.v1/index.ts
@@ -0,0 +1,13 @@
+// THIS FILE IS GENERATED AUTOMATICALLY. DO NOT MODIFY.
+
+import { Registry } from "@cosmjs/proto-signing";
+import { Api } from "./rest";
+
+export const types: any[] = [];
+export const MissingWalletError = new Error("wallet is required");
+
+export const registry = new Registry(<any>types);
+
+export const txClient = {};
+
+export { Api };
