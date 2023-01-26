a70333c9594bba74cb0d6ad75f16d12b76ff9940
codehans <94654388+codehans@users.noreply.github.com>
2023-01-26 11:48:52 +0000

    denomToERC20

--- lib/cjs/gravity/v1/queries.d.ts
+++ lib/cjs/gravity/v1/queries.d.ts
@@ -1,10 +1,11 @@
-import { QueryPendingSendToEth, QueryPendingSendToEthResponse } from "./query";
+import { QueryDenomToERC20Request, QueryDenomToERC20Response, QueryPendingSendToEth, QueryPendingSendToEthResponse } from "./query";
 import { QueryClient } from "@cosmjs/stargate";
 import { Params } from "./genesis";
 export interface GravityExtension {
     readonly gravity: {
         readonly params: () => Promise<Params>;
         readonly pendingSendToEth: (q: QueryPendingSendToEth) => Promise<QueryPendingSendToEthResponse>;
+        readonly denomToERC20: (q: QueryDenomToERC20Request) => Promise<QueryDenomToERC20Response>;
     };
 }
 export declare function setupGravityExtension(base: QueryClient): GravityExtension;
--- src/gravity/v1/queries.ts
+++ src/gravity/v1/queries.ts
@@ -1,6 +1,8 @@
 import { assert } from "@cosmjs/utils";
 import {
   QueryClientImpl,
+  QueryDenomToERC20Request,
+  QueryDenomToERC20Response,
   QueryPendingSendToEth,
   QueryPendingSendToEthResponse,
 } from "./query";
@@ -14,6 +16,9 @@ export interface GravityExtension {
     readonly pendingSendToEth: (
       q: QueryPendingSendToEth
     ) => Promise<QueryPendingSendToEthResponse>;
+    readonly denomToERC20: (
+      q: QueryDenomToERC20Request
+    ) => Promise<QueryDenomToERC20Response>;
   };
 }
 
@@ -30,6 +35,9 @@ export function setupGravityExtension(base: QueryClient): GravityExtension {
       },
       pendingSendToEth: async (q: QueryPendingSendToEth) => {
         return queryService.GetPendingSendToEth(q);
+      },
+      denomToERC20: async (q: QueryDenomToERC20Request) => {
+        return queryService.DenomToERC20(q);
       },
     },
   };
