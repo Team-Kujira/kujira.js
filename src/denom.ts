a4a99435e4952ee24561cbf597137cbf6768f205
codehans <94654388+codehans@users.noreply.github.com>
2023-02-01 13:02:53 +0000

    inj

--- src/denom.ts
+++ src/denom.ts
@@ -362,6 +362,7 @@ export class Denom {
     if (this.symbol === "BOOT") this.decimals = 0;
     if (this.symbol === "DOT") this.decimals = 10;
     if (this.symbol === "SHD") this.decimals = 8;
+    if (this.symbol === "INJ") this.decimals = 18;
     // OKX USDT
     if (
       this.reference ===
