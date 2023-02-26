71aa01d70d637af5bb34e6e9b5a1ed51ab3898a4
codehans <94654388+codehans@users.noreply.github.com>
2023-02-26 12:06:55 +0000

    markets updates

--- src/orca/index.ts
+++ src/orca/index.ts
@@ -99,8 +99,8 @@ export const getMarkets = (
           premiumRatePerSlot: 0.01,
           waitingPeriod: 600,
           markets: [
-            "kujira1fjews4jcm2yx7una77ds7jjjzlx5vgsessguve8jd8v5rc4cgw9s8rlff8",
-            "kujira1m4ves3ymz5hyrj3war3t7uxu9ewt8rwpunja87960n0gre3a5pzspgry4g",
+            "kujira1ecgazyd0waaj3g7l9cmy5gulhxkps2gmxu9ghducvuypjq68mq2smfdslf",
+            "kujira1m0z0kk0qqug74n9u9ul23e28x5fszr628h20xwt6jywjpp64xn4qkxmjq3",
           ],
         },
 
@@ -121,8 +121,8 @@ export const getMarkets = (
           premiumRatePerSlot: 0.01,
           waitingPeriod: 600,
           markets: [
-            "kujira1fjews4jcm2yx7una77ds7jjjzlx5vgsessguve8jd8v5rc4cgw9s8rlff8",
-            "kujira1m4ves3ymz5hyrj3war3t7uxu9ewt8rwpunja87960n0gre3a5pzspgry4g",
+            "kujira1eydneup86kyhew5zqt5r7tkxefr3w5qcsn3ssrpcw9hm4npt3wmqa7as3u",
+            "kujira1hjyjafrt09p4hwsnwch29nrrs40lprfgesqdy44wnp27td872hsse2rree",
           ],
         },
         {
@@ -142,8 +142,8 @@ export const getMarkets = (
           premiumRatePerSlot: 0.01,
           waitingPeriod: 600,
           markets: [
-            "kujira1fjews4jcm2yx7una77ds7jjjzlx5vgsessguve8jd8v5rc4cgw9s8rlff8",
-            "kujira1m4ves3ymz5hyrj3war3t7uxu9ewt8rwpunja87960n0gre3a5pzspgry4g",
+            "kujira1f2jt3f9gzajp5uupeq6xm20h90uzy6l8klvrx52ujaznc8xu8d7s6av27t",
+            "kujira1pep6vkkjexjlsw3y5h4tj27g7s58vkypy8zg7f9qdvlh2992pncqduz84n",
           ],
         },
         {
@@ -184,8 +184,8 @@ export const getMarkets = (
           premiumRatePerSlot: 0.01,
           waitingPeriod: 600,
           markets: [
-            "kujira1fjews4jcm2yx7una77ds7jjjzlx5vgsessguve8jd8v5rc4cgw9s8rlff8",
-            "kujira1m4ves3ymz5hyrj3war3t7uxu9ewt8rwpunja87960n0gre3a5pzspgry4g",
+            "kujira1f2jt3f9gzajp5uupeq6xm20h90uzy6l8klvrx52ujaznc8xu8d7s6av27t",
+            "kujira1pep6vkkjexjlsw3y5h4tj27g7s58vkypy8zg7f9qdvlh2992pncqduz84n",
           ],
         },
         {
@@ -205,8 +205,7 @@ export const getMarkets = (
           premiumRatePerSlot: 0.01,
           waitingPeriod: 600,
           markets: [
-            "kujira1fjews4jcm2yx7una77ds7jjjzlx5vgsessguve8jd8v5rc4cgw9s8rlff8",
-            "kujira1m4ves3ymz5hyrj3war3t7uxu9ewt8rwpunja87960n0gre3a5pzspgry4g",
+            "kujira1twc28l5njc07xuxrs85yahy44y9lw5euwa7kpajc2zdh98w6uyksvjvruq",
           ],
         },
         {
