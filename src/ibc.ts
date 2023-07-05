e0e17f5c866ecbc076a607969d09fc271218fd70
codehans <94654388+codehans@users.noreply.github.com>
2023-07-05 20:27:08 +0100

    ibc

--- src/ibc.ts
+++ src/ibc.ts
@@ -50,6 +50,7 @@ export const TRANSFER_CHANNELS: Record<string, Record<string, string>> = {
     "channel-75": "neutron",
     "channel-89": "teritori",
     "channel-95": "kava",
+    "channel-99": "archway",
   },
 };
 
