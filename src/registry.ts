5ba4fab7e03c690007c88f41cbd69d3b3312f7a6
codehans <94654388+codehans@users.noreply.github.com>
2023-10-22 11:37:01 +0100

    gov v1 types

--- src/registry.ts
+++ src/registry.ts
@@ -14,6 +14,12 @@ import { SoftwareUpgradeProposal } from "cosmjs-types/cosmos/upgrade/v1beta1/upg
 
 import { MsgExecLegacyContent } from "@injectivelabs/core-proto-ts/cjs/cosmos/gov/v1/tx";
 import {
+  MsgPinCodes,
+  MsgUnpinCodes,
+} from "@injectivelabs/core-proto-ts/cjs/cosmwasm/wasm/v1/tx";
+import { MsgUpdateParams } from "cosmjs-types/cosmos/bank/v1beta1/tx";
+import { MsgCommunityPoolSpend } from "cosmjs-types/cosmos/distribution/v1beta1/tx";
+import {
   ClearAdminProposal,
   ExecuteContractProposal,
   InstantiateContractProposal,
@@ -24,6 +30,15 @@ import {
   UpdateAdminProposal,
   UpdateInstantiateConfigProposal,
 } from "cosmjs-types/cosmwasm/wasm/v1/proposal";
+import {
+  MsgClearAdmin,
+  MsgExecuteContract,
+  MsgInstantiateContract,
+  MsgMigrateContract,
+  MsgStoreCode,
+  MsgUpdateAdmin,
+  MsgUpdateInstantiateConfig,
+} from "cosmjs-types/cosmwasm/wasm/v1/tx";
 import { ClientUpdateProposal } from "cosmjs-types/ibc/core/client/v1/client";
 import {
   ClientState,
@@ -46,7 +61,10 @@ const proposalTypes = [
     "/cosmos.distribution.v1beta1.CommunityPoolSpendProposal",
     CommunityPoolSpendProposal,
   ],
+  ["/cosmos.distribution.v1beta1.MsgCommunityPoolSpend", MsgCommunityPoolSpend],
+
   ["/cosmos.params.v1beta1.ParameterChangeProposal", ParameterChangeProposal],
+  ["/cosmos.params.v1beta1.MsgUpdateParams", MsgUpdateParams],
 
   ["/cosmwasm.wasm.v1.StoreCodeProposal", StoreCodeProposal],
   [
@@ -63,6 +81,16 @@ const proposalTypes = [
     "/cosmwasm.wasm.v1.UpdateInstantiateConfigProposal",
     UpdateInstantiateConfigProposal,
   ],
+  ["/cosmwasm.wasm.v1.MsgStoreCode", MsgStoreCode],
+  ["/cosmwasm.wasm.v1.MsgInstantiateContract", MsgInstantiateContract],
+  ["/cosmwasm.wasm.v1.MsgMigrateContract", MsgMigrateContract],
+  ["/cosmwasm.wasm.v1.MsgUpdateAdmin", MsgUpdateAdmin],
+  ["/cosmwasm.wasm.v1.MsgClearAdmin", MsgClearAdmin],
+  ["/cosmwasm.wasm.v1.MsgPinCodes", MsgPinCodes],
+  ["/cosmwasm.wasm.v1.MsgUnpinCodes", MsgUnpinCodes],
+  ["/cosmwasm.wasm.v1.MsgExecuteContract", MsgExecuteContract],
+  ["/cosmwasm.wasm.v1.MsgUpdateInstantiateConfig", MsgUpdateInstantiateConfig],
+
   ["/cosmos.upgrade.v1beta1.SoftwareUpgradeProposal", SoftwareUpgradeProposal],
   ["/cosmos.upgrade.v1beta1.MsgSoftwareUpgrade", MsgSoftwareUpgrade],
   ["/kujira.scheduler.CreateHookProposal", CreateHookProposal],
