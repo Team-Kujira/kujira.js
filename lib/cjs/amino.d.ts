8ac4c0bce0ef6186670897294902756705e091e9
codehans <94654388+codehans@users.noreply.github.com>
2023-11-29 00:31:25 +0000

    amino encoding

--- lib/cjs/amino.d.ts
+++ lib/cjs/amino.d.ts
@@ -1,2 +1,15 @@
+import { AminoMsg } from "@cosmjs/amino";
 import * as s from "@cosmjs/stargate";
+/** Performs an undelegation from a delegate and a validator */
+export interface AminoMsgCancelUnbondingDelegation extends AminoMsg {
+    readonly type: "cosmos-sdk/MsgCancelUnbondingDelegatio";
+    readonly value: {
+        /** Bech32 encoded delegator address */
+        readonly delegator_address: string;
+        /** Bech32 encoded validator address */
+        readonly validator_address: string;
+        readonly amount: s.Coin;
+        readonly creation_height: string;
+    };
+}
 export declare const aminoTypes: (prefix: string) => s.AminoTypes;
--- src/amino.ts
+++ src/amino.ts
@@ -1,6 +1,54 @@
+import { AminoMsg } from "@cosmjs/amino";
 import { createWasmAminoConverters } from "@cosmjs/cosmwasm-stargate";
 import * as s from "@cosmjs/stargate";
+import { assertDefinedAndNotNull } from "@cosmjs/utils";
+import { MsgCancelUnbondingDelegation } from "cosmjs-types/cosmos/staking/v1beta1/tx";
+import Long from "long";
 import { createAuthzAminoConverters } from "./amino/authz";
+
+/** Performs an undelegation from a delegate and a validator */
+export interface AminoMsgCancelUnbondingDelegation extends AminoMsg {
+  readonly type: "cosmos-sdk/MsgCancelUnbondingDelegatio";
+  readonly value: {
+    /** Bech32 encoded delegator address */
+    readonly delegator_address: string;
+    /** Bech32 encoded validator address */
+    readonly validator_address: string;
+    readonly amount: s.Coin;
+    readonly creation_height: string;
+  };
+}
+
+const extra = {
+  "/cosmos.staking.v1beta1.MsgCancelUnbondingDelegation": {
+    aminoType: "cosmos-sdk/MsgCancelUnbondingDelegation",
+    toAmino: ({
+      delegatorAddress,
+      validatorAddress,
+      amount,
+      creationHeight,
+    }: MsgCancelUnbondingDelegation): AminoMsgCancelUnbondingDelegation["value"] => {
+      assertDefinedAndNotNull(amount, "missing amount");
+      return {
+        delegator_address: delegatorAddress,
+        validator_address: validatorAddress,
+        amount: amount,
+        creation_height: creationHeight.toString(),
+      };
+    },
+    fromAmino: ({
+      delegator_address,
+      validator_address,
+      amount,
+      creation_height,
+    }: AminoMsgCancelUnbondingDelegation["value"]): MsgCancelUnbondingDelegation => ({
+      delegatorAddress: delegator_address,
+      validatorAddress: validator_address,
+      amount: amount,
+      creationHeight: Long.fromString(creation_height),
+    }),
+  },
+};
 
 export const aminoTypes = (prefix: string): s.AminoTypes =>
   new s.AminoTypes({
@@ -13,4 +61,5 @@ export const aminoTypes = (prefix: string): s.AminoTypes =>
     ...s.createStakingAminoConverters(),
     ...s.createVestingAminoConverters(),
     ...createWasmAminoConverters(),
+    ...extra,
   });
