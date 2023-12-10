aa20a02a58d76def173e0ddcb1a91247ca03c9e8
codehans <94654388+codehans@users.noreply.github.com>
2023-12-10 20:36:13 +0000

    fix transfers parser

--- lib/esm/parsers.d.ts
+++ lib/esm/parsers.d.ts
@@ -1,6 +1,5 @@
 import { Coin } from "cosmjs-types/cosmos/base/v1beta1/coin";
 import { Event } from "cosmjs-types/tendermint/abci/types";
-export declare function parseCoins(input: string): Coin[];
 export declare const addCoins: (a: Coin[], b: Coin[]) => Coin[];
 export declare const addCoin: (a: Coin[], b: Coin) => Coin[];
 export declare const transfers: (address: string, events: Event[]) => {
--- src/parsers.ts
+++ src/parsers.ts
@@ -1,26 +1,6 @@
-import { BigNumber } from "@ethersproject/bignumber";
+import { parseCoins } from "@cosmjs/stargate";
 import { Coin } from "cosmjs-types/cosmos/base/v1beta1/coin";
 import { Event } from "cosmjs-types/tendermint/abci/types";
-
-export function parseCoins(input: string): Coin[] {
-  return input
-    .replace(/\s/g, "")
-    .split(",")
-    .filter(Boolean)
-    .reduce((agg, part) => {
-      // Denom regex from Stargate (https://github.com/cosmos/cosmos-sdk/blob/v0.42.7/types/coin.go#L599-L601)
-      const match = part.match(/^([0-9]+)([a-zA-Z][a-zA-Z0-9/]{2,127})$/);
-      if (!match) return agg;
-
-      return [
-        {
-          amount: BigNumber.from(match[1]).toString(),
-          denom: match[2],
-        },
-        ...agg,
-      ];
-    }, [] as Coin[]);
-}
 
 export const addCoins = (a: Coin[], b: Coin[]): Coin[] => a.reduce(addCoin, b);
 
@@ -37,40 +17,44 @@ export const addCoin = (a: Coin[], b: Coin): Coin[] => {
       )
     : [b, ...a];
 };
-
 export const transfers = (
   address: string,
   events: Event[]
 ): { send: Coin[]; receive: Coin[] } => {
-  const receive = events
-    .flatMap((e) => (e.type === "coin_received" ? e.attributes : []))
-    .reduce((agg, v, idx, all) => {
-      const key = Buffer.from(v.key).toString();
-      const value = Buffer.from(v.value).toString();
-      if (key === "receiver" && value === address) {
-        // SDK attributes are sorted alphabetically from SDK 47 - we have `amount` and `receiver` here
-        const amount = Buffer.from(all[idx - 1].value).toString();
-        const parsed = parseCoins(amount);
-        return addCoins(agg, parsed);
-      } else {
-        return agg;
+  return events.reduce(
+    (agg: { send: Coin[]; receive: Coin[] }, e) => {
+      if (
+        e.type === "coin_spent" &&
+        e.attributes.find((a) => a.key === "spender")?.value === address
+      ) {
+        return {
+          receive: agg.receive,
+          send: addCoins(
+            agg.send,
+            parseCoins(
+              e.attributes.find((a) => a.key === "amount")?.value || ""
+            )
+          ),
+        };
       }
-    }, [] as Coin[]);
 
-  const send = events
-    .flatMap((e) => (e.type === "coin_spent" ? e.attributes : []))
-    .reduce((agg, v, idx, all) => {
-      const key = Buffer.from(v.key).toString();
-      const value = Buffer.from(v.value).toString();
-      if (key === "spender" && value === address) {
-        // SDK attributes are sorted alphabetically from SDK 47 - we have `amount` and `spender` here
-        const amount = Buffer.from(all[idx - 1].value).toString();
-        const parsed = parseCoins(amount);
-        return addCoins(agg, parsed);
-      } else {
-        return agg;
+      if (
+        e.type === "coin_received" &&
+        e.attributes.find((a) => a.key === "receiver")?.value === address
+      ) {
+        return {
+          send: agg.send,
+          receive: addCoins(
+            agg.send,
+            parseCoins(
+              e.attributes.find((a) => a.key === "amount")?.value || ""
+            )
+          ),
+        };
       }
-    }, [] as Coin[]);
 
-  return { send, receive };
+      return agg;
+    },
+    { send: [], receive: [] }
+  );
 };
