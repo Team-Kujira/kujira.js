8655d38fc0a938ea473ad834fd64b54fc631c26d
codehans <94654388+codehans@users.noreply.github.com>
2024-04-12 15:12:23 +0100

    update pilot defs

--- lib/cjs/pilot.d.ts
+++ lib/cjs/pilot.d.ts
@@ -7,6 +7,33 @@ type SaleResponse = {
     idx: string;
     title: string;
     description: string;
+    url: string;
+    price: string;
+    owner: string;
+    beneficiary: string;
+    amount: {
+        amount: string;
+        denom: string;
+    };
+    status: StatusResponse;
+    orca_address: string;
+    orca_config: {
+        owner: string;
+        markets: [];
+        bid_denom: string;
+        collateral_denom: string;
+        bid_threshold: string;
+        max_slot: number;
+        premium_rate_per_slot: string;
+        waiting_period: 600;
+        liquidation_fee: string;
+        withdrawal_fee: string;
+        fee_address: string;
+    };
+} | {
+    idx: string;
+    title: string;
+    description: string;
     price: string;
     owner: string;
     beneficiary: string;
@@ -33,8 +60,31 @@ type SaleResponse = {
         fee_address: string;
     };
 };
+type StatusResponse = {
+    live: {
+        closes_at: string;
+    };
+} | {
+    retracted: string;
+} | {
+    executed: {
+        at: string;
+        raise_total: string;
+        raise_fee: string;
+        raise_amount: string;
+    };
+};
 export declare const castSale: (network: NETWORK, res: SaleResponse) => Market;
 export type Sale = {
+    idx: string;
+    amount: BigNumber;
+    owner: string;
+    beneficiary: string;
+    price: number;
+    title: string;
+    description: string;
+    status: Status;
+} | {
     idx: string;
     amount: BigNumber;
     owner: string;
@@ -46,6 +96,20 @@ export type Sale = {
     executed: Date | null;
     retracted: Date | null;
     description: string;
+};
+type Status = {
+    live: {
+        closesAt: Date;
+    };
+} | {
+    retracted: Date;
+} | {
+    executed: {
+        at: Date;
+        raiseTotal: BigNumber;
+        raiseFee: BigNumber;
+        raiseAmount: BigNumber;
+    };
 };
 export type Market = Omit<orca.Market, "chain" | "protocol" | "repayDenom" | "type"> & {
     sale: Sale;
--- src/pilot.ts
+++ src/pilot.ts
@@ -6,36 +6,72 @@ import * as orca from "./orca";
 import { KujiraQueryClient } from "./queryClient";
 import contracts from "./resources/contracts.json";
 
-type SaleResponse = {
-  idx: string;
-  title: string;
-  description: string;
-  price: string;
-  owner: string;
-  beneficiary: string;
-  amount: {
-    denom: string;
-    amount: string;
-  };
-  opens: string;
-  closes: string;
-  executed: null | string;
-  retracted: null | string;
-  orca_address: string;
-  orca_config: {
-    owner: string;
-    markets: [];
-    bid_denom: string;
-    collateral_denom: string;
-    bid_threshold: string;
-    max_slot: number;
-    premium_rate_per_slot: string;
-    waiting_period: 600;
-    liquidation_fee: string;
-    withdrawal_fee: string;
-    fee_address: string;
-  };
-};
+type SaleResponse =
+  | {
+      idx: string;
+      title: string;
+      description: string;
+      url: string;
+      price: string;
+      owner: string;
+      beneficiary: string;
+      amount: { amount: string; denom: string };
+      status: StatusResponse;
+      orca_address: string;
+      orca_config: {
+        owner: string;
+        markets: [];
+        bid_denom: string;
+        collateral_denom: string;
+        bid_threshold: string;
+        max_slot: number;
+        premium_rate_per_slot: string;
+        waiting_period: 600;
+        liquidation_fee: string;
+        withdrawal_fee: string;
+        fee_address: string;
+      };
+    }
+  // Legacy, Mainnet
+  | {
+      idx: string;
+      title: string;
+      description: string;
+      price: string;
+      owner: string;
+      beneficiary: string;
+      amount: { denom: string; amount: string };
+      opens: string;
+      closes: string;
+      executed: null | string;
+      retracted: null | string;
+      orca_address: string;
+      orca_config: {
+        owner: string;
+        markets: [];
+        bid_denom: string;
+        collateral_denom: string;
+        bid_threshold: string;
+        max_slot: number;
+        premium_rate_per_slot: string;
+        waiting_period: 600;
+        liquidation_fee: string;
+        withdrawal_fee: string;
+        fee_address: string;
+      };
+    };
+
+type StatusResponse =
+  | { live: { closes_at: string } }
+  | { retracted: string }
+  | {
+      executed: {
+        at: string;
+        raise_total: string;
+        raise_fee: string;
+        raise_amount: string;
+      };
+    };
 
 export const castSale = (network: NETWORK, res: SaleResponse): Market => ({
   label: Denom.from(res.amount.denom).symbol,
@@ -49,38 +85,98 @@ export const castSale = (network: NETWORK, res: SaleResponse): Market => ({
   waitingPeriod: res.orca_config.waiting_period,
   markets: res.orca_config.markets,
   activators: ["kujira1mrwwpea5emn05wmj7d7eweppmw98qj2a642r0e"],
-  sale: {
-    idx: res.idx,
-    title: res.title,
-    owner: res.owner,
-    beneficiary: res.beneficiary,
-    description: res.description,
-    price: parseFloat(res.price),
-    amount: BigNumber.from(res.amount.amount),
-    opens: new Date(parseInt(res.opens) / 1000000),
-    closes: new Date(parseInt(res.closes) / 1000000),
-    executed: res.executed ? new Date(parseInt(res.executed) / 1000000) : null,
-    retracted: res.retracted
-      ? new Date(parseInt(res.retracted) / 1000000)
-      : null,
-  },
+  sale:
+    "status" in res
+      ? {
+          idx: res.idx,
+          title: res.title,
+          owner: res.owner,
+          beneficiary: res.beneficiary,
+          description: res.description,
+          price: parseFloat(res.price),
+          amount: BigNumber.from(res.amount.amount),
+          status:
+            "live" in res.status
+              ? {
+                  live: {
+                    closesAt: new Date(
+                      parseInt(res.status.live.closes_at) / 1000000
+                    ),
+                  },
+                }
+              : "retracted" in res.status
+              ? {
+                  retracted: new Date(parseInt(res.status.retracted) / 1000000),
+                }
+              : {
+                  executed: {
+                    at: new Date(parseInt(res.status.executed.at) / 1000000),
+                    raiseAmount: BigNumber.from(
+                      res.status.executed.raise_amount
+                    ),
+                    raiseTotal: BigNumber.from(res.status.executed.raise_total),
+                    raiseFee: BigNumber.from(res.status.executed.raise_fee),
+                  },
+                },
+        }
+      : {
+          idx: res.idx,
+          title: res.title,
+          owner: res.owner,
+          beneficiary: res.beneficiary,
+          description: res.description,
+          price: parseFloat(res.price),
+          amount: BigNumber.from(res.amount.amount),
+          opens: new Date(parseInt(res.opens) / 1000000),
+          closes: new Date(parseInt(res.closes) / 1000000),
+          executed: res.executed
+            ? new Date(parseInt(res.executed) / 1000000)
+            : null,
+          retracted: res.retracted
+            ? new Date(parseInt(res.retracted) / 1000000)
+            : null,
+        },
   liquidationFee: parseFloat(res.orca_config.liquidation_fee),
   withdrawalFee: parseFloat(res.orca_config.withdrawal_fee),
 });
 
-export type Sale = {
-  idx: string;
-  amount: BigNumber;
-  owner: string;
-  beneficiary: string;
-  opens: Date;
-  closes: Date;
-  price: number;
-  title: string;
-  executed: Date | null;
-  retracted: Date | null;
-  description: string;
-};
+export type Sale =
+  | {
+      idx: string;
+      amount: BigNumber;
+      owner: string;
+      beneficiary: string;
+      price: number;
+      title: string;
+      description: string;
+      status: Status;
+    }
+  // Legacy, mainnet
+  | {
+      idx: string;
+      amount: BigNumber;
+      owner: string;
+      beneficiary: string;
+      opens: Date;
+      closes: Date;
+      price: number;
+      title: string;
+      executed: Date | null;
+      retracted: Date | null;
+      description: string;
+    };
+
+type Status =
+  | { live: { closesAt: Date } }
+  | { retracted: Date }
+  | {
+      executed: {
+        at: Date;
+        raiseTotal: BigNumber;
+        raiseFee: BigNumber;
+        raiseAmount: BigNumber;
+      };
+    };
 
 export type Market = Omit<
   orca.Market,
